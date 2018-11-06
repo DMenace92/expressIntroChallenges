3530f6b71e40e661a85b7e8d70ff6503a033d8b4
Dennis  Enwiya <dennisenwiya@gmail.com>
2018-11-05 21:16:11 -0700

    finished express into

--- part1/server.js
+++ part1/server.js
@@ -5,6 +5,26 @@ var port = process.env.PORT || 8000;
 app.get('/yourroute', function(req, res) {
   res.send("stuff");
 });
+app.get('/hello', function(req,res){
+  res.send('Hello!')
+})
+app.post('/create/:name', function(req, res) {
+  const data = {
+    "id":  1,
+    "name": req.params.name
+  };
+  res.json(data);
+});
+app.get('/', function(req, res) {
+  res.sendFile(__dirname+"/index.html");
+});
+
+app.get('/verify/:age', function(req, res) {
+  if (req.params.age > 13) {
+    res.sendStatus(200);
+  }
+  res.sendStatus(403);
+});
 
 app.use(function(req, res) {
   res.sendStatus(404);
--- part2/server.js
+++ part2/server.js
@@ -6,6 +6,38 @@ app.get('/yourroute', function(req, res) {
   res.send("stuff");
 });
 
+app.post('/create/:name/:age', function(req, res) {
+  const users = fs.readFileSync('./storage.json', 'utf8');
+  const usersArr = users ? JSON.parse(users) : [];
+  const userData = {
+    "id" : usersArr.length + 1,
+    "name": req.params.name,
+    "age": + req.params.age
+  };
+  usersArr.push(userData);
+  fs.writeFileSync('./storage.json', JSON.stringify(usersArr));
+  res.sendStatus(200);
+});
+
+app.get('/', function(req, res) {
+  res.json(JSON.parse(fs.readFileSync('./storage.json', 'utf8')));
+});
+
+
+app.get('/:nameOrId', function(req, res) {
+  const users = fs.readFileSync('./storage.json', 'utf8');
+  if (users) {
+    const usersArr = JSON.parse(users);
+    for (const user of usersArr) {
+      if (user.name === req.params.nameOrId || user.id == req.params.nameOrId) {
+        res.json(user);
+      }
+    }
+  }
+  res.sendStatus(400);
+});
+
+
 app.use(function(req, res) {
   res.sendStatus(404);
 });
